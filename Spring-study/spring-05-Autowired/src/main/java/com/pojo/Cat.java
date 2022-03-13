0e647cd86d8a82855df51d5fc5242793772b9340
Chen Qiu <qiu.chen1@northeastern.edu>
2022-03-12 19:18:56 -0800

    update autowire

---
+++ Spring-study/spring-05-Autowired/src/main/java/com/pojo/Cat.java
@@ -0,0 +1,9 @@
+package com.pojo;
+
+public class Cat {
+
+    public void makeSound() {
+        System.out.println("Meow~");
+    }
+
+}
---
+++ Spring-study/spring-05-Autowired/src/main/java/com/pojo/Dog.java
@@ -0,0 +1,8 @@
+package com.pojo;
+
+public class Dog {
+
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
