515db1316e669d23f82ffa9de040a3afb1c90cc7
Chen Qiu <qiu.chen1@northeastern.edu>
2022-03-20 21:31:24 -0700

    update mybatis

---
+++ Spring-study/spring-10-mybatis/src/main/java/com/chen/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.chen.mapper;
+
+import com.chen.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    public List<User> selectUser();
+}
---
+++ Spring-study/spring-10-mybatis/src/test/java/MyTest.java
@@ -0,0 +1,31 @@
+import com.chen.mapper.UserMapper;
+import com.chen.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MyTest {
+
+    @Test
+    public void test() throws IOException {
+        String resources = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resources);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlsession = sqlSessionFactory.openSession();
+
+        UserMapper mapper = sqlsession.getMapper(UserMapper.class);
+
+        List<User> userList = mapper.selectUser();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlsession.close();
+    }
+}
